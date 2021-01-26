5fbad8bb29145081aaa7f2b7e0c96aa54d906963
wangshengbo <760698932@qq.com>
2021-01-26 09:45:43 +0800

    update

--- base_models.py
+++ base_models.py
@@ -1,5 +1,6 @@
 import sys
-import xlsxwriter
+# import xlsxwriter
+import pickle
 
 def tryPyQt5():
     from PyQt5 import QtWidgets
@@ -32,7 +33,33 @@ def trySys():
     print('batch: ', batch, 'type: ', type(batch))
 
 
-if __name__ == '__main__':
-    # trySys()
-    tryPyQt5()
+def result_code():
+    print("in")
+    res = [200, 1,  {"clinic_code": 200, "result_oode":300, "Id": 2, "StatusFlag": 0}]
+    out_register_info = res[2]
+    if res[0] == 200:
+        dto = {
+        }
+        if res[2]['StatusFlag'] == 0:
+            # 未挂号, 需要进行挂号处理
+            sync_res = [200, 1, 1,{"clinic_code": 200, "result_oode":300}]
+            if sync_res[0] == 200:
+                result_data = sync_res[2]
+                if result_data:
+                    register_no = result_data.get('clinic_code', None)
+                    print(register_no)
+                    dto.update({"RegisterNo": register_no})
+                    # result_code = 4则不要update OutRegisters里的RegisterNo
+                    result_code = result_data.get('result_code', None)
+                    print("inout")
 
+    print("out")
+
+def try_pickle():
+    f = open("test.pkl", "rb")
+    data = pickle.load(f)
+    print(data)
+
+if __name__ == '__main__':
+    pass
+    try_pickle()
