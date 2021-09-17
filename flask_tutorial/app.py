b4f167a84beef1c6ae0547307723d60367690307
wangshengbo <760698932@qq.com>
2021-09-17 14:33:45 +0800

    update

--- flask_tutorial/app.py
+++ flask_tutorial/app.py
@@ -48,10 +48,29 @@ class Article(db.Model):
         return f"Article<{self.title}>"
 
 
+class Writer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    books = db.relationship("Book", back_populates="writer")
+
+    def __repr__(self) -> str:
+        return f"Writer<{self.name}>"
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(50), index=True)
+    writer_id=db.Column(db.Integer, db.ForeignKey("writer.id"))
+    writer = db.relationship("Writer", back_populates="books")
+
+    def __repr__(self) -> str:
+        return f"Book<{self.title}>"
+
+
 # flask shell 自动加载return模块
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, Note=Note, Author=Author, Article=Article)
+    return dict(db=db, Note=Note, Author=Author, Article=Article, Writer=Writer, Book=Book)
 
 
 # 在终端通过命令 flask initdb 执行
