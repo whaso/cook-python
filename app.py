f7a6d3aa4953530cc12bcdb1a5827ab40754fc7d
wangshengbo <760698932@qq.com>
2021-12-29 15:15:04 +0800

    update

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    from flask import request
+    print(request.headers)
+    return {"ok": "0k"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
