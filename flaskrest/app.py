d3494475caea397a240c8710e9a55561ff4940eb
bog <email>
2022-07-04 15:15:13 +0800

    socket io try

--- flaskrest/app.py
+++ flaskrest/app.py
@@ -1,9 +1,14 @@
+import time
+
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_socketio import SocketIO, emit
 
 
 app = Flask(__name__)
+
 api = Api(app)
+socketio = SocketIO(app)
 
 
 class HelloWorld(Resource):
@@ -12,4 +17,24 @@ class HelloWorld(Resource):
         return {"hello": "World"}
 
 
+@socketio.on('request_for_response',namespace='/testnamespace')
+def give_response(data):
+    value = data.get('param')
+ 
+    #进行一些对value的处理或者其他操作,在此期间可以随时会调用emit方法向前台发送消息
+    emit('response',{'code':'200','msg':'start to process...'})
+ 
+    time.sleep(1)
+    emit('response',{'code':'200','msg':'processed'})
+
+
+# @app.route("")
+# def hello():
+#     return {"hello": "world"}
+
 api.add_resource(HelloWorld, "/")
+
+
+if __name__ == "__main__":
+    socketio.run(app)
+    pass
