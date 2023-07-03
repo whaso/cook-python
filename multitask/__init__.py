6d76e13e293e2d089825a1386f6415f220bc6dc6
Bo <760698932@qq.com>
2023-07-03 17:37:30 +0800

    update

--- multitask/__init__.py
+++ multitask/__init__.py
@@ -6,21 +6,23 @@ my_list = []
 
 
 def read_val():
-    print(f"reading list: {my_list}")
+    while True:
+        print(f"reading list: {my_list}")
 
 
 def write_val():
-    print("writing....")
-    for i in range(3):
-        time.sleep(0.5)
-        my_list.append(i)
-        print(f"writing val: {i}")
+    while True:
+        print("writing....")
+    # for i in range(3):
+    #     time.sleep(0.5)
+    #     my_list.append(i)
+    #     print(f"writing val: {i}")
     print(f"writed.....{my_list}")
 
 
-read_task = multiprocessing.Process(target=read_val)
+if __name__ == "__main__":
+    read_task = multiprocessing.Process(target=read_val)
+    write_task = multiprocessing.Process(target=write_val)
 
-write_task = multiprocessing.Process(target=write_val)
-
-read_task.run()
-write_task.run()
+    write_task.start()
+    read_task.start()
